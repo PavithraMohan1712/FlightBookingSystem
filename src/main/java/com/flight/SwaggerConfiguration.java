fb9793faa0f2551b3029583f903da7e6dd793616
PavithraMohan1712 <87612704+PavithraMohan1712@users.noreply.github.com>
2021-10-19 18:11:57 +0530

    Update SwaggerConfiguration.java

--- src/main/java/com/flight/SwaggerConfiguration.java
+++ src/main/java/com/flight/SwaggerConfiguration.java
@@ -22,14 +22,14 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfiguration {
 	
-	private static final String BASIC_AUTH = "basicAuth";
+	//private static final String BASIC_AUTH = "basicAuth";
 	
 	@Bean
 	public Docket api() {
-		HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme
-	            .BASIC_AUTH_BUILDER
-	            .name("Basic Authentication")
-	            .build();
+// 		HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme
+// 	            .BASIC_AUTH_BUILDER
+// 	            .name("Basic Authentication")
+// 	            .build();
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.flight"))
@@ -42,17 +42,17 @@ public class SwaggerConfiguration {
 	public ApiInfo apiEndPointInfo() {
 		return new ApiInfoBuilder().title("Flight Booking")
 				.description("flight booking app")
-				.contact(new Contact("Project","https://project.com","pavithramohan1712@gmail.com"))
+				//.contact(new Contact("Project","https://project.com","pavithramohan1712@gmail.com"))
 				.licenseUrl("http://www.apache.org/licenses/LICENCE-2.0.html")
 				.version("0.0.1-SNAPSHOT")
 				.build();
 	}
 
-	   private SecurityContext securityContext() {
-	       return SecurityContext.builder().securityReferences(Arrays.asList(basicAuthReference())).build();
-	   }
-	   private SecurityReference basicAuthReference() {
-	       return new SecurityReference(BASIC_AUTH, new AuthorizationScope[0]);
-	   }
+// 	   private SecurityContext securityContext() {
+// 	       return SecurityContext.builder().securityReferences(Arrays.asList(basicAuthReference())).build();
+// 	   }
+// 	   private SecurityReference basicAuthReference() {
+// 	       return new SecurityReference(BASIC_AUTH, new AuthorizationScope[0]);
+// 	   }
 
 }
